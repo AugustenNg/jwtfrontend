61b49e43db0d025309787ca027f70e0e8995676b
UIT-23730172-HUY <23730172@ms.uit.edu.vn>
2025-11-11 11:58:10 +0700

    responsive login page

---
+++ src/component/Login/login.js
@@ -0,0 +1,40 @@
+import './login.scss'
+
+const Login = (props) => {
+    return (<div className="login-container">
+        <div className="container pt-3">
+            <div className="row px-3 px-sm-0">
+                <div className="content-left align-content-center col-12 col-sm-7 d-sm-block d-none">
+                    <div className="brand">
+                        HUY NGUYEN
+                    </div>
+                    <div className="details">
+                        HUY help you to find job, connect with us to get more information
+                    </div>
+                </div>
+                <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 ">
+                    <div className="brand d-sm-none d-block text-center">
+                        HUY NGUYEN
+                    </div>
+                    <input type="text" className="form-control" placeholder="Email or your username"/>
+                    <input type="password" className="form-control" placeholder="Password"/>
+                    <button className="login btn btn-primary" type="submit">
+                        Login
+                    </button>
+                    <span className="text-center">
+                            <a href="#" className="forgot-password">
+                              Forgot your password
+                            </a>
+                        </span>
+                    <hr/>
+                    <div className="text-center">
+                        <button className="register btn btn-success mb-3" type="submit">
+                            Create a new account
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>);
+}
+export default Login;
